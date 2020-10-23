8f55d75d4c45bfe5e4746bc3217e3ef52132380d
Neo <neo@tiefkuehler-madness.me>
2020-10-23 23:45:14 +0200

    display connection count

--- gatekeeper.py
+++ gatekeeper.py
@@ -28,7 +28,7 @@ class gatekeeper:
             #Any source > 350 connections within 5 minutes
             if len(data) / 5 > 350:
                 message = self.prepareMessage(data,True)
-                self.notify(src+" exceeded 350 Connections",message)
+                self.notify(src+" exceeded 350 ("+str(len(data) / 5)+") Connections",message)
 
     def prepareMessage(self,data,short=False):
         rows,count = "",0

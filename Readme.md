c454ec5c3ccced94f3cf6ee1e0d84d08a7d4573c
Neo <neo@tiefkuehler-madness.me>
2020-10-23 17:47:55 +0200

    base

---
+++ Readme.md
@@ -0,0 +1,11 @@
+#gatekeeper
+
+Watch the bloody gates
+
+**Dependencies**<br />
+```
+apt-get install -y pmacct python3
+```
+
+**Prepare**<br />
+Move pmacctd.conf to /etc/pmacctd.conf and restart
---
+++ gatekeeper.py
@@ -0,0 +1,58 @@
+#!/usr/bin/python3
+import requests, json
+
+ports = {'21':5, '22':5, '25':5, '80':2, '443':2, '6667':2, '6881':500}
+network = []
+
+class gatekeeper:
+    def __init__(self):
+        global network
+        print("Loading pmacct")
+        with open('/tmp/pmacct.json', 'r') as f:
+            network = f.read()
+
+    def sortBySource(self,rows):
+        source = {}
+        for row in rows:
+            try:
+                tmp = source[row['ip_src']]
+                tmp = tmp + (row,)
+                source[row['ip_src']] = tmp
+            except:
+                source[row['ip_src']] = {}
+                source[row['ip_src']] = (row,)
+        return source
+
+    def triggers(self,source):
+        for src,data in source.items():
+            #Any source > 300 connections within 5 minutes
+            if len(data) > 300:
+                self.notify(src+" exceeded 300 Connections",data)
+
+    def notify(self,title,message):
+        #Gotify
+        params = (('token', '<apptoken>'))
+        payload = {
+                    'title': (None, title),
+                    'message': (None, message),
+                    'priority': (None, '5')
+                  }
+        response = requests.post('https://push.example.de/message', params=params, files=payload)
+
+    def run(self):
+        print("Parsing pmacct")
+        rows = []
+        for row in network.split('\n'):
+            #Drop empty lines
+            if row.strip() == "": continue
+            line = json.loads(row)
+            #Drop Multicast traffic
+            if '239.255.255.' in line['ip_dst']: continue
+            if '224.0.0.' in line['ip_dst']: continue
+            if 'ff02::1' in line['ip_dst']: continue
+            rows.append(line)
+        source = self.sortBySource(rows)
+        self.triggers(source)
+
+Gate = gatekeeper()
+Gate.run()
